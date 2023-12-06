5a68cf2a34e7a3fce8dbe1a9c3a63d99ed18af60
ebunOyemade <125628914+ebunOyemade@users.noreply.github.com>
2023-12-06 10:29:03 -0500

    Update App.js

--- search-bar-practice-react/src/App.js
+++ search-bar-practice-react/src/App.js
@@ -1,102 +1,138 @@
-//https://dev.to/asimdahall/simple-search-form-in-react-using-hooks-42pg
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
+import React, { useState, useEffect } from 'react';
+import Navbar from "../Components/Navbar/Navbar.jsx";
+import Product from '../Components/Product/Product.js';
+import FilterSidebar from './FilterSidebar.js';
 
-// dummy data to test our search on
-const product = [
-  { label: 'product 1', value: 1.99 },
+import './Shop.css';
 
-   { label: 'product 2', value: 2.99 },
-
-   {label: 'product 2', value: 3.99 },
-];
-
-
-const store = [
-  "Restuarant 1",
-  "Restuarant 2",
-  "Restuarant 3",
+const Shop = () => {
+    const [cartItems, setCartItems] = useState([]);
+    const [filterOption, setFilterOption] = useState(null);
+    const [products, setProducts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [value, setValue] = React.useState(0);
+    const [searchResults, setSearchResults] = useState([]);
+    const handleChange = e => {
+      setSearchTerm(e.target.value.toLowerCase());
+    };
+   
+    useEffect(() => {
+      fetch('http://localhost:5000/api/products')
+        .then(response => response.json())
+        .then(data => {
+          if (data.products) {
+            console.log('Received products:', data.products);
+            setProducts(data.products);
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching products:', error);
+        });
+        const results = products.filter((products) =>
+      products.toLowerCase().includes(searchTerm),
+    );
+    setSearchResults(results);
+    }, [products,searchTerm]);
   
-];
-
-const handleChange = (event) => {
+    const addToCart = (product) => {
+        // Check if the item is already in the cart
+        const existingItem = cartItems.find(item => item.id === product.id);
+      
+        if (existingItem) {
+          // If the item is already in the cart, update the quantity
+          const updatedCartItems = cartItems.map(item =>
+            item.id === existingItem.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          );
+      
+          setCartItems(updatedCartItems);
+        } else {
+          // If the item is not in the cart, add a new item with quantity 1
+          setCartItems(prevCartItems => [...prevCartItems, { ...product, quantity: 1 }]);
+        }
+      
+        // Update the cart on the server
+        fetch('http://localhost:5000/api/add_to_cart', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(product),
+        })
+          .then(response => response.json())
+          .then(data => {
+            if (data.success) {
+              console.log('Item added to the cart on the server side');
+            } else {
+              console.error('Failed to add item to the cart:', data.message);
+            }
+          })
+          .catch(error => {
+            console.error('Error adding item to the cart:', error);
+          });
+      };
+      
   
-  setValue(event.target.value);
+    const handleClearCart = () => {
+      fetch('http://localhost:5000/api/clear_cart', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.success) {
+            setCartItems([]);
+            console.log('Cart cleared successfully');
+          } else {
+            console.error('Error clearing cart:', data.message);
+          }
+        })
+        .catch(error => {
+          console.error('Error clearing cart:', error);
+        });
 
+    };
+  
+    const handleFilter = (option) => {
+      setFilterOption(option);
+      // Implement filtering logic
+    };
+
+  return (
+    <>
+      <Navbar cartCount={cartItems.length} />
+        
+      <div className="content-container">
+        <div className="sidebar">
+          <FilterSidebar className="filter-sidebar" />
+        </div>
+        <div className = "search">
+          <input type = "text" placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}>
+            
+          </input>
+        </div>
+        <div className="products-grid">
+          {searchResults.map((product) => (
+            <Product
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              imageSource={product.image_source}
+              addToCart={addToCart} // Pass addToCart function to Product component
+            />
+          ))}
+        </div>
+      </div>
+
+
+    </>
+  );
 };
 
-
-export default function App () {
-  // take a value from the user and save it to a state
-  const [searchTerm, setSearchTerm] = useState("");
-  const [value, setValue] = React.useState(0);
-  const [searchResults, setSearchResults] = useState([]);
-  //const [menu, setMenu] = useState("");;
-  
-  // at every occurance of the change event, sets the current value of the form to the state
-  const handleChange = e => {
-    setSearchTerm(e.target.value.toLowerCase());
-  };
-  useEffect(() => {
-    
-    const results = store.filter(store =>
-      store.toLowerCase().includes(searchTerm)
-    
-    );
-    
-    setSearchResults(results);
-  }, [searchTerm]);
-  // ^ props.people along with searchTerm in dependency
-  // rendering a search bar (input) and a list of items to search through
-  return (
-    <div className="App">
-      <input
-        type="text"
-        placeholder="Search"
-        value={searchTerm}
-        onChange={handleChange}
-      />
-      <ul>
-        {searchResults.map(item => (
-          <li>{item} <Dropdown
-          options={product}
-   
-          value={value}
-   
-          onChange={handleChange}
-   
-        />
-        <li><button>Add to cart</button></li>
-        </li>
-        ))}
-        
-      </ul>
-      
-    </div>
-  );
-}
-const Dropdown = ({ value, options, onChange }) => {
-
-  return (
- 
-    <label>
- 
-      <select value={value} onChange={onChange}>
- 
-        {options.map((option) => (
- 
-          <option value={option.value}>{option.label} ${option.value}</option>
-          
-        ))}
- 
-      </select>
- 
-    </label>
- 
-  );
- 
- };
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+export default Shop;
